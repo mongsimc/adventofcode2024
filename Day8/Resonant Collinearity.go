0ca2351e2b16bdd511f8611f0f6dd79d6550c054
chongms <chongms@sg.ibm.com>
2024-12-23 12:08:38 +0800

    update

--- Day8/Resonant Collinearity.go
+++ Day8/Resonant Collinearity.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"time"
+
+	aoc "github.com/mongsimc/adventofcode2024/utils"
 )
 
 func main() {
 	t1 := time.Now()
 
-	answer1 := SomeFunction1()
-	answer2 := SomeFunction2()
+	lines := aoc.ReadFile("puzzle.input")
+
+	answer1 := SomeFunction1(lines)
+	answer2 := SomeFunction2(lines)
 
 	fmt.Printf("Answer1: %d\n", answer1)
 	fmt.Printf("Answer2: %d\n", answer2)
@@ -19,10 +23,10 @@ func main() {
 	fmt.Printf("Time Taken: %v\n", t2.Sub(t1))
 }
 
-func SomeFunction1() (answer int) {
+func SomeFunction1(input []string) (answer int) {
 	return
 }
 
-func SomeFunction2() (answer int) {
+func SomeFunction2(input []string) (answer int) {
 	return
 }
